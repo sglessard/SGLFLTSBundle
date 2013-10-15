d949cc51369451a2e6417748cb057f5accf827df
Simon <s.g.lessard@gmail.com>
2013-10-15 13:58:11 -0400

    travis-ci

--- Tests/Controller/DefaultControllerTest.php
+++ Tests/Controller/DefaultControllerTest.php
@@ -10,8 +10,11 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        $crawler = $client->request('GET', '/');
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("href=\"/login\"")')->count()
+        );
     }
 }
