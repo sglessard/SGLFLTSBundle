53819aca0e5fabf4bf4bc99936798bfeca6c6d8c
Simon <s.g.lessard@gmail.com>
2013-10-15 13:14:28 -0400

    Test : bootstrap without mongo db

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -3,4 +3,4 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 \Doctrine\Common\Annotations\AnnotationRegistry::registerLoader('class_exists');
-\Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver::registerAnnotationClasses();
+// \Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver::registerAnnotationClasses();
