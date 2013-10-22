bd3273c8e2e3a6c4514e32a57ea0e73f8a7c3de2
Simon <s.g.lessard@gmail.com>
2013-10-22 13:05:19 -0400

    CanadaTax tests

---
+++ Tests/Util/Tax/CanadaTaxTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace SGL\FLTSBundle\Tests\Util\Tax;
+
+use SGL\FLTSBundle\Util\Tax\CanadaTax;
+
+class CalculatorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testImportParameterFromArray()
+    {
+        $values = array(
+            'gst'=>array(5.00),
+            'pst'=>array(2010=>7.875, 2011=>8.925, 2012=>9.975),
+            'hst'=>array()
+        );
+
+        $canTax = new CanadaTax();
+        $canTax->setDefaultGst($values['gst']);
+        $canTax->setDefaultPst($values['pst']);
+        $canTax->setDefaultHst($values['hst']);
+
+        $result = 0;
+        if ($canTax->getGst() === 5.00)
+            $result++;
+        if ($canTax->getPst() === 9.975)
+            $result++;
+        if ($canTax->getHst() === 0.00)
+            $result++;
+
+
+        // vérifie que votre classe a correctement calculé!
+        $this->assertTrue($canTax->getGst() === 5.000);
+        $this->assertTrue($canTax->getPst() === 9.975);
+        $this->assertTrue($canTax->getHst() === 0);
+    }
+}
